010a4cffaa2de705a623c71bf1aa1c72bab88812
marek <marek@192.168.56.1>
2020-01-02 17:00:13 +0100

    class added

---
+++ MKL25Z128xxx4_Project_ACC/source/communication/CircularBufferUart.h
@@ -0,0 +1,34 @@
+/*
+ * CircularBufferUart.h
+ *
+ *  Created on: Jan 2, 2020
+ *      Author: marek
+ */
+
+/*
+ * this source is from https://github.com/hudikm/TPRIS_CIRCULAR_BUFFER.git
+ */
+
+#ifndef COMMUNICATION_CIRCULARBUFFERUART_H_
+#define COMMUNICATION_CIRCULARBUFFERUART_H_
+
+#include <fsl_lpsci.h>
+#include <communication/CircularBuffer.h>
+
+#define _bufferUartSize 150
+#define BAUDRATE 57600
+extern buffer_t cBuffer;
+
+/**
+ * function read one bytes from received buffer
+ * @param pointer to variable where received char will by return
+ * @return 0 when not success (empty buffer/ nothing to read) not 0 when success received char (not empty buffer)
+ */
+uint8_t get_char(uint8_t* ch);
+
+/**
+ * function initialize circular buffer and interupt from UART
+ */
+void CBufferUart_Init();
+
+#endif /* COMMUNICATION_CIRCULARBUFFERUART_H_ */
